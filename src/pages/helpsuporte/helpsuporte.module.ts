e3d78c900b149c7b17e187be785a5644d7a60158
Rodrigo Monteiro <rodrigo.monteiro.nascimento@gmail.com>
2017-08-17 21:09:44 -0300

    novas page

---
+++ src/pages/helpsuporte/helpsuporte.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HelpsuportePage } from './helpsuporte';
+
+@NgModule({
+  declarations: [
+    HelpsuportePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HelpsuportePage),
+  ],
+})
+export class HelpsuportePageModule {}
